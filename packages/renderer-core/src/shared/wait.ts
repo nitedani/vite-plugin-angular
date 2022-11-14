02f229be5f9a5da2c64dce1ab7850c6e36d5678b
Dániel Horváth <horvath.daniel@lexunit.hu>
2022-11-14 23:39:21 +0100

    update

--- packages/renderer-core/src/shared/wait.ts
+++ packages/renderer-core/src/shared/wait.ts
@@ -1,31 +1,20 @@
-import {
-  firstValueFrom,
-  from,
-  isObservable,
-  Observable,
-  ObservableInput,
-} from 'rxjs';
+import { defer, from, ObservableInput, tap } from 'rxjs';
 
-export const wait = <T, O extends Promise<T> | Observable<T>>(input: O) => {
+export const wait = <T, O extends Promise<T>>(input: O) => {
   if (import.meta.env.SSR) {
-    const i = setInterval(() => {});
-    let promise: Promise<T>;
-    if (isObservable(input)) {
-      promise = firstValueFrom<T>(input);
-    } else {
-      promise = input;
-    }
-    promise.finally(() => clearInterval(i));
+    const i = setTimeout(() => {}, 10000);
+    input.finally(() => clearTimeout(i));
+  } else {
+    return input;
   }
-  return input;
 };
 
 export const wait$ = <T, O extends ObservableInput<T>>(input: O) => {
   const obs = from(input);
   if (import.meta.env.SSR) {
-    const i = setInterval(() => {});
-    const promise = firstValueFrom<T>(obs);
-    promise.finally(() => clearInterval(i));
+    const i = setTimeout(() => {}, 10000);
+    return defer(() => obs.pipe(tap(() => clearTimeout(i))));
+  } else {
+    return obs;
   }
-  return obs;
 };
--- packages/vite-plugin-angular/src/plugin/plugins/dev.plugin.ts
+++ packages/vite-plugin-angular/src/plugin/plugins/dev.plugin.ts
@@ -1,6 +1,48 @@
 import { Plugin } from 'vite';
 import { swcTransform } from '../swc/transform.js';
 
+const hmrCode = `
+let __appRef;
+const __bootstrapApplication = (...args) => bootstrapApplication(...args).then(appRef =>{
+  __appRef = appRef;
+  return appRef;
+});
+if (import.meta.hot) {
+  import('@nitedani/vite-plugin-angular/hmr').then(
+    ({ createInputTransfer, createNewHosts, removeNgStyles }) => {
+      //@ts-ignore
+      import.meta.hot.accept(() => {
+        //@ts-ignore
+        const store = import.meta.hot.data.store;
+        if (!store) return;
+        if ('restoreInputValues' in store) {
+          store.restoreInputValues();
+        }
+        __appRef.tick();
+        store.disposeOldHosts();
+        delete store.disposeOldHosts;
+        delete store.state;
+        delete store.restoreInputValues;
+      });
+      //@ts-ignore
+      import.meta.hot.dispose(() => {
+        const cmpLocation = __appRef.components.map(
+          (cmp) => cmp.location.nativeElement
+        );
+        const store = {};
+        //@ts-ignore
+        store.disposeOldHosts = createNewHosts(cmpLocation);
+        //@ts-ignore
+        store.restoreInputValues = createInputTransfer();
+        //@ts-ignore
+        import.meta.hot.data.store = store;
+        removeNgStyles();
+      });
+    }
+  );
+}
+`;
+
 export const DevelopmentPlugin: Plugin = {
   name: 'vite-plugin-angular-dev',
   enforce: 'pre',
@@ -15,6 +57,23 @@ export const DevelopmentPlugin: Plugin = {
     };
   },
   transform(code, id) {
+    //TODO: do this better
+    const isEntry = id.endsWith('main.ts');
+
+    if (isEntry) {
+      let t = 0;
+      let found = false;
+      code = code.replace(/bootstrapApplication/g, match => {
+        if (++t === 2) {
+          found = true;
+          return '__bootstrapApplication';
+        }
+        return match;
+      });
+      if (found) {
+        code = hmrCode + code;
+      }
+    }
     // Run everything else through SWC
     // On the server, we need decorator metadata,
     // @analogjs/vite-plugin-angular uses esbuild, but esbuild doesn't support decorator metadata
