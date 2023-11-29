dd319a5786292a3999a4091c40ac88e6205a314c
Dániel Horváth <horvath.daniel@lexunit.hu>
2023-11-29 20:37:11 +0100

    add defer example

--- examples/universal/src/app/app.component.ts
+++ examples/universal/src/app/app.component.ts
@@ -1,16 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
+
+@Component({
+  selector: 'heavy-component',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    HEAVY LOADED:
+    <p>{{ title }}</p>
+  `,
+})
+export class HeavyComponent implements OnInit {
+  title: string = 'Heavy Component';
+
+  ngOnInit(): void {
+    this.title = 'Heavy Component Loaded';
+  }
+}
 
 @Component({
   standalone: true,
   selector: 'app-component',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [HeavyComponent],
 })
 export class AppComponent {
   counter = 0;
-  constructor(private appService: AppService, private httpClient: HttpClient) {
+  constructor(
+    private appService: AppService,
+    private httpClient: HttpClient,
+  ) {
     httpClient.get('/api').subscribe((data) => {
       console.log(data);
     });
